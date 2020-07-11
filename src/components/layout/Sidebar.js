646a9cf8ae4f4d0d2b50149e0b72adb41daa5361
Prashant-SK <psk.dev.ca@gmail.com>
2020-07-11 16:19:09 -0700

    modified project component

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -5,6 +5,7 @@ import {
   FaRegCalendarAlt,
   FaRegCalendar,
 } from "react-icons/fa";
+import { Projects } from "../Projects";
 import { useSelectedProjectValue } from "../../context";
 
 export const Sidebar = () => {
@@ -39,8 +40,8 @@ export const Sidebar = () => {
         </span>
         <h2>Projects</h2>
       </div>
-      <ul className="sidebar__projects">TODO: Projects will be here</ul>
-      Add project Name here!
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+      <Projects />
     </div>
   );
 };
